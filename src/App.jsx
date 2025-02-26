34b2e540543fdd0b106cf515497b769f3b5b9d44
Ylenia <yleniamighela@gmail.com>
2025-02-26 16:00:09 +0100

    added default

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// importo i componenti
+
+
+// importo le pages
+import PostPage from "./components/PostPage.jsx";
+
+
+// importo il LAYOUT
+import DefaultLayout from "./layout/DefaultLayout.jsx";
+
 
 function App() {
 
 
   return (
+
     <>
-      <h1>CIAO</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />} >
+            <Route path="/" element={<PostPage />} />
+            {/* <Route path="/post" element={<PostsLista />} />
+            <Route path="/aggiungipost/:id" element={<PostsDetailsPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
+
   )
 }
 
