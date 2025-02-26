a45958e4f1365cc04301a114eee63dd6db142ce9
Ylenia <yleniamighela@gmail.com>
2025-02-26 16:14:55 +0100

    added components

---
+++ src/components/PostPage.jsx
@@ -0,0 +1,13 @@
+// importo la lista dei post
+
+import PostsList from "./PostsList";
+
+export default function PostPage() {
+    return (
+        <>
+            <h2>Menù dei dolci </h2>
+            <PostsList />
+        </>
+
+    );
+}
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
+
+export default function PostsLista() {
+
+
+
+    const [posts, setPost] = useState([]);
+
+
+    //funzione di gestione chiamate API
+    function fetchPosts() {
+        axios.get("http://localhost:3000/posts/")
+            .then((res) => {
+                setPost(res.data);
+            })
+    }
+
+    useEffect(fetchPosts, []);
+
+
+    return (
+
+        <>
+
+            {posts.map((post) => (
+
+                <div className='container' key={post.id} >
+                    <div className='postsitem'>
+                        <h2>{post.title}</h2>
+                        <img src={post.image} alt={post.title} />
+                        <p>{post.content}</p>
+                        <br />
+                        <span>{post.tags.join(",")}</span>
+                    </div>
+
+                </div >
+            ))}
+        </>
+    )
+
+}
+
